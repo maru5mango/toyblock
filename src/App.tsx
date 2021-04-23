45bcf77325e077d3612cc0050639e11b41983c4b
maru5mango <mjel0507@gmail.com>
2021-04-23 14:14:39 +0900

    mainInfo Page

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from "react-router-dom";
 import GridCard from './components/GridCard';
+import mainPage from './components/mainPage';
 
 const App = () => {
   return(
     <Switch>
-      <Route exact path="/" component={GridCard} />
+      <Route exact path="/grid" component={GridCard} />
+      <Route exact path="/main" component={mainPage} />
     </Switch>
   )
 }
